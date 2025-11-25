7522469141b2453aebf8eedc6b8097570b661268
kevwpl <51119253+kevwpl@users.noreply.github.com>
2025-11-25 22:51:35 +0100

    New qr-code component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,7 +17,7 @@
         PhoneCall,
         GithubIcon,
         SearchIcon, ImagePlus, Book, SquarePlay, Calendar, Palette, MousePointer2, RectangleHorizontal,
-        RectangleEllipsis, CircleDot,
+        RectangleEllipsis, CircleDot, QrCode,
     } from "lucide-svelte";
     import { page } from '$app/state';
     import { goto } from "$app/navigation";
@@ -44,6 +44,7 @@
                 { href: "/components/image-zoom", label: "Image Zoom", icon: ImagePlus },
                 { href: "/components/number-ticker", label: "Number Ticker", icon: ClockArrowUp },
                 { href: "/components/phone-input", label: "Phone Input", icon: PhoneCall },
+                { href: "/components/qr-code", label: "QR Code", icon: QrCode },
                 { href: "/components/status-dot", label: "Status Dot", icon: CircleDot },
                 { href: "/components/stepper", label: "Stepper", icon: ArrowBigRightDash },
                 { href: "/components/shiny-button", label: "Shiny Button", icon: Sparkle },
